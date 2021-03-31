b00038b84d354a84f200cee80ba52b96ce71f432
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-30 22:44:31 -0300

    Add Rollbar

--- app.js
+++ app.js
@@ -13,14 +13,25 @@ app.use(function (req, res, next) {
 })
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+if(process.env.ROLLBAR_ACCESS_TOKEN) {
+  const Rollbar = require("rollbar")
+  const rollbar = new Rollbar({
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true
+  })
 
-  // render the error page
-  res.status(err.status || 500)
-  res.status(err.status || 500).send(err)
-})
+  app.use(rollbar.errorHandler())
+} else {
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.status(err.status || 500).send(err)
+  })
+}
 
 module.exports = app
