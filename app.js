62b8eea5b24b5cf3a83adc5fc9627f06d5a9654c
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-30 22:13:18 -0300

    Add debug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const http = require('http')
+const debug = require('debug')('ecomanda-delivery:server')
 
 const PORT = process.env.PORT || '5000'
 
@@ -9,8 +10,39 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end('Hello World')
-});
+})
 
 server.listen(PORT, hostname, () => {
   console.log(`Server running at http://${hostname}:${PORT}/`)
-});
+})
+server.on('error', onError)
+server.on('listening', onListening)
+
+function onError (error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof PORT === 'string'
+    ? 'Pipe ' + PORT
+    : 'Port ' + PORT
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+    default:
+      throw error
+  }
+}
+
+function onListening () {
+  const addr = server.address()
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'PORT ' + addr.PORT
+  debug('Listening on ' + bind)
+}
