abb7b80484c3ff899fa925be70b0252a414790c8
Alan Costa Facchini <costafacchini@gmail.com>
2021-04-28 01:22:07 -0300

    Create phone normalizer

---
+++ app/helpers/normalize-phone.js
@@ -0,0 +1,24 @@
+class NormalizePhone {
+  constructor(number) {
+    this.number = number.replace(/[^0-9.]/g, '')
+    this.type = number.includes('@g.us') ? '@g.us' : '@c.us'
+    this.#normalize()
+  }
+
+  #normalize() {
+    if (this.number[this.number.length - 1] === '.') {
+      this.number = this.number.slice(0, this.number.length - 1)
+    }
+    if (this.number.length <= 9) {
+      this.number = ''
+    }
+    if (this.number.length === 10 || this.number.length === 11) {
+      this.number = '55' + this.number
+    }
+    if (this.number.length === 12) {
+      this.number = [this.number.slice(0, 4), '9', this.number.slice(4)].join('')
+    }
+  }
+}
+
+module.exports = NormalizePhone
