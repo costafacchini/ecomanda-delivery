085b964661656b6e6cfa95d199bc774b878164b8
Alan Costa Facchini <costafacchini@gmail.com>
2021-05-20 22:41:03 -0300

    Move redis URI to env var

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis')
 
-const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+const REDIS_URL = process.env.REDIS_URL
 
 const redis = new Redis(REDIS_URL)
 
