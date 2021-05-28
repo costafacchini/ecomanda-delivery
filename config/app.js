2f849d09585db3dc14a6fd293b57e6dfbc926f48
Alan Costa Facchini <costafacchini@gmail.com>
2021-05-27 23:30:54 -0300

    Increase the size of files accepted by express

--- config/app.js
+++ config/app.js
@@ -9,8 +9,8 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: false }))
 app.use(logger('dev'))
 connect()
 enableCors(app)
