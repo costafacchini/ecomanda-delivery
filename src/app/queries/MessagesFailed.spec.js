9fadaf26d392d1d17a03f6d1a08e72c2da452586
Alan Costa Facchini <costafacchini@gmail.com>
2021-07-08 12:36:43 -0300

    wip

--- src/app/queries/MessagesFailed.spec.js
+++ src/app/queries/MessagesFailed.spec.js
@@ -2,9 +2,9 @@ const mongoServer = require('../../../.jest/utils')
 const Licensee = require('@models/Licensee')
 const Contact = require('@models/Contact')
 const Message = require('@models/Message')
-const MessagesSendedQuery = require('./MessagesSended')
+const MessagesFailedQuery = require('./MessagesFailed')
 
-describe('MessagesSendedQuery', () => {
+describe('MessagesFailedQuery', () => {
   beforeEach(async () => {
     await mongoServer.connect()
   })
@@ -12,38 +12,38 @@ describe('MessagesSendedQuery', () => {
   afterEach(async () => {
     await mongoServer.disconnect()
   })
-  it('returns the messages that sended filtered by licensee and period', async () => {
+  it('returns the messages that not sended filtered by licensee and period', async () => {
     const filteredLicensee = await Licensee.create({ name: 'Alcateia', licenseKind: 'demo' })
     const filteredContact = await Contact.create({
       number: '551190283745',
       talkingWithChatBot: false,
       licensee: filteredLicensee._id,
     })
-    const filteredMessageSended1 = await Message.create({
+    const filteredMessageNotSended1 = await Message.create({
       text: 'Message 1',
       number: filteredContact.number,
       contact: filteredContact._id,
       licensee: filteredLicensee._id,
       destination: 'to-chat',
-      sended: true,
+      sended: false,
       createdAt: new Date(2021, 6, 3, 0, 0, 0),
     })
-    const filteredMessageSended2 = await Message.create({
+    const filteredMessageNotSended2 = await Message.create({
       text: 'Message 2',
       number: filteredContact.number,
       contact: filteredContact._id,
       licensee: filteredLicensee._id,
       destination: 'to-chat',
-      sended: true,
+      sended: false,
       createdAt: new Date(2021, 6, 3, 23, 59, 58),
     })
-    const filteredMessageNotSended = await Message.create({
+    const filteredMessageSended = await Message.create({
       text: 'Message 3',
       number: filteredContact.number,
       contact: filteredContact._id,
       licensee: filteredLicensee._id,
       destination: 'to-chat',
-      sended: false,
+      sended: true,
       createdAt: new Date(2021, 6, 3, 23, 59, 58),
     })
     const filteredMessageBefore = await Message.create({
@@ -52,7 +52,7 @@ describe('MessagesSendedQuery', () => {
       contact: filteredContact._id,
       licensee: filteredLicensee._id,
       destination: 'to-chat',
-      sended: true,
+      sended: false,
       createdAt: new Date(2021, 6, 2, 23, 59, 59),
     })
     const filteredMessageAfter = await Message.create({
@@ -61,7 +61,7 @@ describe('MessagesSendedQuery', () => {
       contact: filteredContact._id,
       licensee: filteredLicensee._id,
       destination: 'to-chat',
-      sended: true,
+      sended: false,
       createdAt: new Date(2021, 6, 4, 0, 0, 0),
     })
     const anotherLicensee = await Licensee.create({ name: 'Alcateia', licenseKind: 'demo' })
@@ -71,23 +71,33 @@ describe('MessagesSendedQuery', () => {
       contact: filteredContact._id,
       licensee: anotherLicensee._id,
       destination: 'to-chat',
-      sended: true,
+      sended: false,
       createdAt: new Date(2021, 6, 3, 0, 0, 0),
     })
+    const filteredMessageNotSendedChatEndedByAgent = await Message.create({
+      text: 'Chat encerrado pelo agente',
+      number: filteredContact.number,
+      contact: filteredContact._id,
+      licensee: filteredLicensee._id,
+      destination: 'to-chat',
+      sended: false,
+      createdAt: new Date(2021, 6, 3, 23, 59, 58),
+    })
 
-    const messagesSendedQuery = new MessagesSendedQuery(
+    const messagesFailedQuery = new MessagesFailedQuery(
       new Date(2021, 6, 3, 0, 0, 0),
       new Date(2021, 6, 3, 23, 59, 59),
       filteredLicensee._id
     )
-    const records = await messagesSendedQuery.all()
+    const records = await messagesFailedQuery.all()
 
     expect(records.length).toEqual(2)
-    expect(records).toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageSended1._id })]))
-    expect(records).toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageSended2._id })]))
+    expect(records).toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageNotSended1._id })]))
+    expect(records).toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageNotSended2._id })]))
     expect(records).not.toEqual(
-      expect.arrayContaining([expect.objectContaining({ _id: filteredMessageNotSended._id })])
+      expect.arrayContaining([expect.objectContaining({ _id: filteredMessageNotSendedChatEndedByAgent._id })])
     )
+    expect(records).not.toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageSended._id })]))
     expect(records).not.toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageBefore._id })]))
     expect(records).not.toEqual(expect.arrayContaining([expect.objectContaining({ _id: filteredMessageAfter._id })]))
     expect(records).not.toEqual(
