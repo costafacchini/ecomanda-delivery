b5bf16909026b380b8084e9ee8d643c3af6da986
Alan Costa Facchini <costafacchini@gmail.com>
2021-07-21 17:47:39 -0300

    Fix services

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,46 @@
-export default async function api(url) {
-  const response = await fetch(url)
+export default function api() {
+  async function request(url, method, { headers, body }) {
+    const requestOptions = {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    }
 
-  return response.ok ? response.json() : response.text()
+    let result
+
+    try {
+      const response = await fetch(url, requestOptions)
+      const data = await response.text()
+
+      result = {
+        status: response.status,
+        data: data.length > 0 && isJSON(response) ? JSON.parse(data) : data,
+      }
+    } catch (e) {
+      result = {
+        status: 500,
+        data: `Ocorreu um erro ao tentar dar um post na url ${url} com o body ${JSON.stringify(
+          body
+        )} e resultou na mensagem ${e}`,
+      }
+    }
+
+    return result
+  }
+
+  function isJSON(response) {
+    return response.headers.get('content-type')?.includes('application/json')
+  }
+
+  return {
+    post: function (url, requestOpts = {}) {
+      return request(url, 'post', requestOpts)
+    },
+    get: function (url, requestOpts = {}) {
+      return request(url, 'get', requestOpts)
+    }
+  }
 }
