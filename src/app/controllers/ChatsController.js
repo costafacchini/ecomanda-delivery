841cd4f7e0aa97739e7f4c10d05e06b025586087
Alan Costa Facchini <costafacchini@gmail.com>
2021-09-21 09:32:28 -0300

    Catch

--- src/app/controllers/ChatsController.js
+++ src/app/controllers/ChatsController.js
@@ -4,11 +4,16 @@ const queueServer = require('@config/queue')
 class ChatsController {
   async message(req, res) {
     console.info(`Mensagem chegando do plugin de chat para ${req.licensee.name}: ${JSON.stringify(req.body)}`)
-    const body = await Body.create({ content: req.body, licensee: req.licensee._id })
 
-    await queueServer.addJob('chat-message', { bodyId: body._id })
+    try {
+      const body = await Body.create({ content: req.body, licensee: req.licensee._id })
 
-    res.status(200).send({ body: 'Solicitação de mensagem para a plataforma de chat agendado' })
+      await queueServer.addJob('chat-message', { bodyId: body._id })
+
+      res.status(200).send({ body: 'Solicitação de mensagem para a plataforma de chat agendado' })
+    } catch (err) {
+      res.status(500).send({ body: err.toString() })
+    }
   }
 }
 
