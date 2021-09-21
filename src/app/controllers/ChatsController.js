4d68fd5bf7fec153d252c86b352b3a18141e9a6b
Alan Costa Facchini <costafacchini@gmail.com>
2021-09-21 12:00:44 -0300

    remove unnecessary code

--- src/app/controllers/ChatsController.js
+++ src/app/controllers/ChatsController.js
@@ -3,18 +3,13 @@ const queueServer = require('@config/queue')
 
 class ChatsController {
   async message(req, res) {
-    console.info(`Mensagem chegando do plugin de chat para ${req.licensee.name}: ${JSON.stringify(req.body)}`)
+    console.info(`Mensagem chegando do plugin de chat: ${JSON.stringify(req.body)}`)
+    const body = new Body({ content: req.body, licensee: req.licensee._id })
+    await body.save()
 
-    try {
-      const body = await Body.create({ content: req.body, licensee: req.licensee._id })
+    await queueServer.addJob('chat-message', { bodyId: body._id })
 
-      await queueServer.addJob('chat-message', { bodyId: body._id })
-
-      res.status(200).send({ body: 'Solicitação de mensagem para a plataforma de chat agendado' })
-    } catch (err) {
-      console.info(`Erro ao gravar o body: ${err.toString()}`)
-      res.status(500).send({ body: err.toString() })
-    }
+    res.status(200).send({ body: 'Solicitação de mensagem para a plataforma de chat agendado' })
   }
 }
 
