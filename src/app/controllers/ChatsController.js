880043a152d415014bc02694b978cae657d31c7e
Alan Costa Facchini <costafacchini@gmail.com>
2021-09-21 14:16:16 -0300

    remove dada unnecessary

--- src/app/controllers/ChatsController.js
+++ src/app/controllers/ChatsController.js
@@ -3,16 +3,19 @@ const queueServer = require('@config/queue')
 
 class ChatsController {
   async message(req, res) {
-    console.info(`Mensagem chegando do plugin de chat para ${req.licensee.name}: ${JSON.stringify(req.body)}`)
+    const tempBody = req.body
+    delete tempBody['crmData']
+
+    console.info(`Mensagem chegando do plugin de chat para ${req.licensee.name}: ${JSON.stringify(tempBody)}`)
 
     try {
-      const body = await Body.create({ content: req.body, licensee: req.licensee._id })
+      const body = await Body.create({ content: tempBody, licensee: req.licensee._id })
 
       await queueServer.addJob('chat-message', { bodyId: body._id })
 
       res.status(200).send({ body: 'Solicitação de mensagem para a plataforma de chat agendado' })
     } catch (err) {
-      console.info(`Erro ao gravar o body: ${err.toString()} ${JSON.stringify(req.body)}`)
+      console.info(`Erro ao gravar o body: ${err.toString()} ${JSON.stringify(tempBody)}`)
       res.status(500).send({ body: err.toString() })
     }
   }
