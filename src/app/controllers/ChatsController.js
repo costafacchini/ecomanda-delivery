e0a08e0b27b9be108f972bbc031a6cf592e29fde
Alan Costa Facchini <costafacchini@gmail.com>
2021-09-22 08:39:13 -0300

    Remove very large tag sent by Rocketchat

--- src/app/controllers/ChatsController.js
+++ src/app/controllers/ChatsController.js
@@ -3,11 +3,14 @@ const queueServer = require('@config/queue')
 
 class ChatsController {
   async message(req, res) {
-    console.info(`Mensagem chegando do plugin de chat: ${JSON.stringify(req.body)}`)
-    const body = new Body({ content: req.body, licensee: req.licensee._id })
-    await body.save()
+    const { body } = req
+    // Remove crmData because of Rocketchat send a higher history inside the body
+    delete body['crmData']
 
-    await queueServer.addJob('chat-message', { bodyId: body._id })
+    console.info(`Mensagem chegando do plugin de chat: ${JSON.stringify(body)}`)
+    const bodySaved = await Body.create({ content: body, licensee: req.licensee._id })
+
+    await queueServer.addJob('chat-message', { bodyId: bodySaved._id })
 
     res.status(200).send({ body: 'Solicitação de mensagem para a plataforma de chat agendado' })
   }
