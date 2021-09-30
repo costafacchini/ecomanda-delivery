7ff254e298ef8fd7db19f1366142e326f5b1148b
Alan Costa Facchini <costafacchini@gmail.com>
2021-09-30 08:23:19 -0300

    Add webhook config on Dialog360 whatsapp plugin

--- src/app/plugins/messengers/Dialog.js
+++ src/app/plugins/messengers/Dialog.js
@@ -212,6 +212,18 @@ class Dialog {
       data: response.data,
     }
   }
+
+  async setWebhook(url, token) {
+    const headers = { 'D360-API-KEY': token }
+
+    const body = {
+      url: `${this.licensee.urlWhatsappWebhook}`,
+    }
+
+    const response = await request.post(`${url}v1/configs/webhook`, { headers, body })
+
+    return response.status === 200
+  }
 }
 
 module.exports = Dialog
