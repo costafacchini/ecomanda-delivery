7dcc4fc919ef0e3e07fd6b7675a4e227a17d0f9c
Alan Costa Facchini <costafacchini@gmail.com>
2021-11-05 09:48:02 -0300

    Test redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,26 +3,19 @@ const Redis = require('ioredis')
 const REDIS_URL = process.env.REDIS_TLS_URL || process.env.REDIS_URL
 
 const createRedis = function () {
-  if (process.env.NODE_ENV === 'test') {
-    return new Redis({
-      port: process.env.REDIS_PORT,
-      host: process.env.REDIS_HOST,
+  if (REDIS_URL.includes('rediss')) {
+    return new Redis(REDIS_URL, {
+      tls: {
+        rejectUnauthorized: false,
+      },
+      enableReadyCheck: false,
+      maxRetriesPerRequest: null,
     })
   } else {
-    if (REDIS_URL.includes('rediss')) {
-      return new Redis(REDIS_URL, {
-        tls: {
-          rejectUnauthorized: false,
-        },
-        enableReadyCheck: false,
-        maxRetriesPerRequest: null,
-      })
-    } else {
-      return new Redis(REDIS_URL, {
-        enableReadyCheck: false,
-        maxRetriesPerRequest: null,
-      })
-    }
+    return new Redis(REDIS_URL, {
+      enableReadyCheck: false,
+      maxRetriesPerRequest: null,
+    })
   }
 }
 
