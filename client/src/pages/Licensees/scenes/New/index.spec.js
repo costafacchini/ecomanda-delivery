8ac9cc0d9b263c89c94840767b89b8faf91add45
Alan Costa Facchini <costafacchini@gmail.com>
2021-11-10 01:01:29 -0300

    Fix tests

--- client/src/pages/Licensees/scenes/New/index.spec.js
+++ client/src/pages/Licensees/scenes/New/index.spec.js
@@ -1,12 +1,17 @@
-import LicenseeNew from ".";
+import LicenseeNew from '.'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
 import { createLicensee } from '../../../../services/licensees'
 
 jest.mock('../../../../services/licensees')
 
 describe('<LicenseeNew />', () => {
   function mount() {
-    render(<LicenseeNew />)
+    render(
+      <MemoryRouter>
+        <LicenseeNew />
+      </MemoryRouter>
+    )
   }
 
   it('creates a new licensee when the backend returns success', async () => {
