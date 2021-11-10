86f499707a3641db73fdacf728108d3bfa7bb0d3
Alan Costa Facchini <costafacchini@gmail.com>
2021-11-10 00:51:17 -0300

    Change navbar style (#300)

--- client/src/pages/Navbar/index.js
+++ client/src/pages/Navbar/index.js
@@ -3,7 +3,7 @@ import { logout } from '../../services/auth'
 
 export default function Navbar({ loggedUser }) {
   return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -30,10 +30,10 @@ export default function Navbar({ loggedUser }) {
               <li className="nav-item">
                 <a className="nav-link" href="/#/sendmessages">Enviar mensagem</a>
               </li> */}
-              <li className="nav-item justify-content-end">
-                <a className="nav-link" href="/" onClick={logout}>Sair</a>
-              </li>
             </ul>
+            <div className="btn-item">
+              <a className="btn btn-primary" href="/" onClick={logout}>Sair</a>
+            </div>
           </div>
         </div>
       </nav>
