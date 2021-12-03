7e4acaa9e7a50604a892682b05fe494ab5cc4ad0
Alan Costa Facchini <costafacchini@gmail.com>
2021-12-02 21:43:11 -0300

    Create Trigger

---
+++ src/app/factories/trigger.js
@@ -0,0 +1,36 @@
+const { Factory } = require('fishery')
+const { licensee } = require('./licensee')
+
+const triggerMultiProduct = Factory.define(() => ({
+  name: 'Send multi products',
+  expression: 'send_multi_product',
+  triggerKind: 'multi_product',
+  catalogMulti: 'catalog',
+  licensee: licensee.build(),
+}))
+
+const triggerSingleProduct = Factory.define(() => ({
+  name: 'Send single product',
+  expression: 'send_single_product',
+  triggerKind: 'single_product',
+  catalogSingle: 'product',
+  licensee: licensee.build(),
+}))
+
+const triggerReplyButton = Factory.define(() => ({
+  name: 'Send reply buttons',
+  expression: 'send_reply_buttons',
+  triggerKind: 'reply_button',
+  textReplyButton: 'buttons',
+  licensee: licensee.build(),
+}))
+
+const triggerListMessage = Factory.define(() => ({
+  name: 'Send list message',
+  expression: 'send_list_message',
+  triggerKind: 'list_message',
+  messagesList: 'list',
+  licensee: licensee.build(),
+}))
+
+module.exports = { triggerMultiProduct, triggerSingleProduct, triggerReplyButton, triggerListMessage }
