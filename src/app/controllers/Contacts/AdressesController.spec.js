8372c047a66696ff2f423f150c1bacdf59ca0965
Alan Costa Facchini <costafacchini@gmail.com>
2021-12-15 17:48:19 -0300

    Create addresses fields on contacts

---
+++ src/app/controllers/Contacts/AdressesController.spec.js
@@ -0,0 +1,173 @@
+const Licensee = require('@models/Licensee')
+const Contact = require('@models/Contact')
+const request = require('supertest')
+const mongoServer = require('../../../../.jest/utils')
+const { expressServer } = require('../../../../.jest/server-express')
+const { licensee: licenseeFactory } = require('@factories/licensee')
+const { contact: contactFactory } = require('@factories/contact')
+
+describe('contact controller', () => {
+  let licensee
+
+  beforeAll(async () => {
+    await mongoServer.connect()
+
+    licensee = await Licensee.create(licenseeFactory.build())
+  })
+
+  afterAll(async () => {
+    await mongoServer.disconnect()
+  })
+
+  describe('about auth', () => {
+    it('returns status 401 and message if query param token is not valid', async () => {
+      await request(expressServer)
+        .get('/api/v1/contacts/address/48999083624?token=627365264')
+        .expect('Content-Type', /json/)
+        .expect(401, { message: 'Token não informado ou inválido.' })
+    })
+
+    it('returns status 401 and message if query param token is informed', async () => {
+      await request(expressServer)
+        .get('/api/v1/contacts/address')
+        .expect('Content-Type', /json/)
+        .expect(401, { message: 'Token não informado ou inválido.' })
+    })
+  })
+
+  describe('update', () => {
+    describe('response', () => {
+      it('returns status 200 and the contact address data if the update is successful', async () => {
+        await Contact.create(
+          contactFactory.build({
+            number: '5511990283745',
+            name: 'John Doe',
+            type: '@c.us',
+            licensee,
+          })
+        )
+
+        await request(expressServer)
+          .post(`/api/v1/contacts/address/11990283745?token=${licensee.apiToken}`)
+          .send({
+            address: 'Rua dois de outubro',
+            address_number: '123',
+            address_complement: 'rooms 1 and 2',
+            neighborhood: 'Pedra branca',
+            city: 'São Paulo',
+            cep: '98543287',
+            uf: 'SP',
+            delivery_tax: 10.39,
+          })
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body.name).toEqual('John Doe')
+            expect(response.body.number).toEqual('5511990283745')
+            expect(response.body.type).toEqual('@c.us')
+            expect(response.body.address).toEqual('Rua dois de outubro')
+            expect(response.body.address_number).toEqual('123')
+            expect(response.body.address_complement).toEqual('rooms 1 and 2')
+            expect(response.body.neighborhood).toEqual('Pedra branca')
+            expect(response.body.city).toEqual('São Paulo')
+            expect(response.body.uf).toEqual('SP')
+            expect(response.body.cep).toEqual('98543287')
+            expect(response.body.delivery_tax).toEqual(10.39)
+            expect(response.body.licensee).toEqual(licensee._id.toString())
+          })
+      })
+
+      it('returns status 404 and message if the contact is not founded', async () => {
+        await request(expressServer)
+          .post(`/api/v1/contacts/address/1111111?token=${licensee.apiToken}`)
+          .expect('Content-Type', /json/)
+          .expect(404, { errors: { message: 'Contato 1111111 não encontrado' } })
+      })
+
+      it('returns status 500 and message if the some error ocurred when update the contact', async () => {
+        const contactFindOneSpy = jest.spyOn(Contact, 'findOne').mockImplementation(() => {
+          throw new Error('some error')
+        })
+
+        await Contact.create(contactFactory.build({ number: '5511990283745', licensee }))
+
+        await request(expressServer)
+          .post(`/api/v1/contacts/address/11990283745?token=${licensee.apiToken}`)
+          .send({ address: 'Address modified' })
+          .expect('Content-Type', /json/)
+          .expect(500, {
+            errors: { message: 'Error: some error' },
+          })
+
+        contactFindOneSpy.mockRestore()
+      })
+    })
+  })
+
+  describe('show', () => {
+    describe('response', () => {
+      it('returns status 200 and message if contact exists', async () => {
+        const contact = await Contact.create(
+          contactFactory.build({
+            name: 'John Doe',
+            type: '@c.us',
+            licensee,
+            address: 'Rua dois de outubro',
+            address_number: '123',
+            address_complement: 'rooms 1 and 2',
+            neighborhood: 'Pedra branca',
+            city: 'São Paulo',
+            cep: '98543287',
+            uf: 'SP',
+            delivery_tax: 10.39,
+          })
+        )
+
+        await request(expressServer)
+          .get(`/api/v1/contacts/address/${contact.number}?token=${licensee.apiToken}`)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body.name).toEqual('John Doe')
+            expect(response.body.number).toEqual('5511990283745')
+            expect(response.body.type).toEqual('@c.us')
+            expect(response.body.address).toEqual('Rua dois de outubro')
+            expect(response.body.address_number).toEqual('123')
+            expect(response.body.address_complement).toEqual('rooms 1 and 2')
+            expect(response.body.neighborhood).toEqual('Pedra branca')
+            expect(response.body.city).toEqual('São Paulo')
+            expect(response.body.uf).toEqual('SP')
+            expect(response.body.cep).toEqual('98543287')
+            expect(response.body.delivery_tax).toEqual(10.39)
+            expect(response.body.licensee._id.toString()).toEqual(licensee._id.toString())
+          })
+      })
+
+      it('returns status 404 and message if contact does not exists', async () => {
+        await request(expressServer)
+          .get(`/api/v1/contacts/address/111111?token=${licensee.apiToken}`)
+          .expect('Content-Type', /json/)
+          .expect(404, {
+            errors: { message: 'Contato 111111 não encontrado' },
+          })
+      })
+
+      it('returns status 500 and message if occurs another error', async () => {
+        const contactFindOneSpy = jest.spyOn(Contact, 'findOne').mockImplementation(() => {
+          throw new Error('some error')
+        })
+
+        const contact = await Contact.create(contactFactory.build({ number: '5511990283745', licensee }))
+
+        await request(expressServer)
+          .get(`/api/v1/contacts/address/${contact.number}?token=${licensee.apiToken}`)
+          .expect('Content-Type', /json/)
+          .expect(500, {
+            errors: { message: 'Error: some error' },
+          })
+
+        contactFindOneSpy.mockRestore()
+      })
+    })
+  })
+})
