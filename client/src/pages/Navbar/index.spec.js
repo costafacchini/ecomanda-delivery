6fef4ede00187e3762474d3bf7e49319ad30affa
Alan Costa Facchini <costafacchini@gmail.com>
2022-06-29 18:54:20 -0300

    Fix tests [2]

--- client/src/pages/Navbar/index.spec.js
+++ client/src/pages/Navbar/index.spec.js
@@ -9,12 +9,22 @@ describe('<Navbar>', () => {
     expect(screen.getByText(/Contatos/i)).toBeInTheDocument()
     expect(screen.getByText(/Gatilhos/i)).toBeInTheDocument()
     expect(screen.getByText(/Mensagens/i)).toBeInTheDocument()
+    expect(screen.getByText(/Templates/i)).toBeInTheDocument()
+    expect(screen.queryByText(/Licenciados/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Usuários/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Relatórios/i)).not.toBeInTheDocument()
   })
 
   it('renders the navbar options for admins', () => {
     render(<Navbar loggedUser={{ isAdmin: true }} />)
 
     expect(screen.getByText(/Licenciados/i)).toBeInTheDocument()
+    expect(screen.getByText(/Usuários/i)).toBeInTheDocument()
+    expect(screen.queryByText(/Relatórios/i)).not.toBeInTheDocument()
+  })
+
+  it('renders the navbar options for admins and super users', () => {
+    render(<Navbar loggedUser={{ isAdmin: true, isSuper: true }} />)
     expect(screen.getByText(/Relatórios/i)).toBeInTheDocument()
   })
 })
--- client/src/pages/Users/scenes/New/index.spec.js
+++ client/src/pages/Users/scenes/New/index.spec.js
@@ -1,11 +1,11 @@
-import ContactNew from '.'
+import UserNew from '.'
 import { fireEvent, render, screen, waitFor, cleanup } from '@testing-library/react'
 import { MemoryRouter } from 'react-router'
-import { createContact } from '../../../../services/contact'
+import { createUser } from '../../../../services/user'
 
-jest.mock('../../../../services/contact')
+jest.mock('../../../../services/user')
 
-describe('<ContactNew />', () => {
+describe('<UserNew />', () => {
   let loggedUser = {
     isSuper: false,
     licensee: 'id'
@@ -14,34 +14,25 @@ describe('<ContactNew />', () => {
   function mount() {
     render(
       <MemoryRouter>
-        <ContactNew loggedUser={loggedUser} />
+        <UserNew loggedUser={loggedUser} />
       </MemoryRouter>)
   }
 
-  it('creates a new contact when the backend returns success', async () => {
+  it('creates a new user when the backend returns success', async () => {
     mount()
 
-    createContact.mockResolvedValue({ status: 201 })
+    createUser.mockResolvedValue({ status: 201 })
 
     fireEvent.click(screen.getByRole('button', { name: 'Salvar' }))
 
-    await waitFor(() => expect(createContact).toHaveBeenCalledWith({
+    await waitFor(() => expect(createUser).toHaveBeenCalledWith({
       name: '',
-      number: '',
       email: '',
-      talkingWithChatBot: false,
-      licensee: 'id',
-      waId: '',
-      landbotId: '',
-      address: '',
-      address_number: '',
-      address_complement: '',
-      neighborhood: '',
-      city: '',
-      cep: '',
-      ud: '',
-      delivery_tax: 0,
-      plugin_cart_id: ''
+      password: '',
+      active: true,
+      isAdmin: false,
+      isSuper: false,
+      licensee: 'id'
     }))
 
     cleanup()
@@ -51,34 +42,25 @@ describe('<ContactNew />', () => {
     }
     mount()
 
-    createContact.mockResolvedValue({ status: 201 })
+    createUser.mockResolvedValue({ status: 201 })
 
     fireEvent.click(screen.getByRole('button', { name: 'Salvar' }))
 
-    await waitFor(() => expect(createContact).toHaveBeenCalledWith({
+    await waitFor(() => expect(createUser).toHaveBeenCalledWith({
       name: '',
-      number: '',
       email: '',
-      talkingWithChatBot: false,
-      licensee: '',
-      waId: '',
-      landbotId: '',
-      address: '',
-      address_number: '',
-      address_complement: '',
-      neighborhood: '',
-      city: '',
-      cep: '',
-      ud: '',
-      delivery_tax: 0,
-      plugin_cart_id: ''
+      password: '',
+      active: true,
+      isAdmin: false,
+      isSuper: false,
+      licensee: ''
     }))
   })
 
   it('renders the errors when the backend returns error', async () => {
     mount()
 
-    createContact.mockResolvedValue({ status: 422, data: { errors: [{ message: 'This is an error' }] } })
+    createUser.mockResolvedValue({ status: 422, data: { errors: [{ message: 'This is an error' }] } })
 
     fireEvent.click(screen.getByRole('button', { name: 'Salvar' }))
 
