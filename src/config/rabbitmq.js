757916b48fb9878e72380d34f5cef889a5fb6107
Alan Costa Facchini <costafacchini@gmail.com>
2022-09-05 22:24:17 -0300

    Add RabbitMQ on app and reset chatbots on them

---
+++ src/config/rabbitmq.js
@@ -0,0 +1,39 @@
+const amqp = require('amqplib/callback_api')
+const RABBIT_URL = process.env.CLOUDAMQP_URL
+const resetChatbots = require('../app/services/ResetChatbots')
+
+function publishMessage(payload) {
+  amqp.connect(RABBIT_URL, function (errorOnConnect, connection) {
+    if (errorOnConnect) throw errorOnConnect
+
+    connection.createChannel(function (errorOnCreateChannel, channel) {
+      if (errorOnCreateChannel) throw errorOnCreateChannel
+
+      channel.assertQueue('main', { durable: false })
+
+      channel.sendToQueue('main', Buffer.from(payload))
+    })
+  })
+}
+
+function consumeChannel() {
+  amqp.connect(RABBIT_URL, function (errorOnConnect, connection) {
+    if (errorOnConnect) throw errorOnConnect
+
+    connection.createChannel(function (errorOnCreateChannel, channel) {
+      if (errorOnCreateChannel) throw errorOnCreateChannel
+
+      channel.assertQueue('main', { durable: false })
+
+      channel.consume(
+        'main',
+        async function (_) {
+          await resetChatbots()
+        },
+        { noAck: true }
+      )
+    })
+  })
+}
+
+module.exports = { publishMessage, consumeChannel }
