147835988564a5f243bcbeea6af083b834271a37
Alan Costa Facchini <costafacchini@gmail.com>
2023-09-15 19:38:22 -0300

    Create page to view integration logs

--- client/src/pages/Reports/routes.js
+++ client/src/pages/Reports/routes.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import BillingIndex from './Billing/scenes/Index'
+import IntegrationlogIndex from './Integrationlog/scenes/Index'
+import { AppContext } from '../../contexts/App'
 
 function ReportsRoutes() {
+  const { currentUser } = useContext(AppContext)
+
   return (
     <Routes>
       <Route path='/billing' element={<BillingIndex />} />
+      <Route path='/integrationlog' element={<IntegrationlogIndex currentUser={currentUser} />} />
     </Routes>
   )
 }
--- src/app/websockets/reports/billing/index.js
+++ src/app/websockets/reports/billing/index.js
@@ -1,5 +1,6 @@
 const { io } = require('@config/http')
 const BillingQuery = require('@queries/BillingQuery')
+const IntegrationlogsQuery = require('@queries/IntegrationlogsQuery')
 
 io.on('connect', (socket) => {
   socket.on('load_billing_report', async (params) => {
@@ -9,5 +10,19 @@ io.on('connect', (socket) => {
     const records = await billingQuery.all()
 
     io.emit('send_billing_report', { data: records })
+  })
+
+  socket.on('load_integrationlog', async (params) => {
+    const { startDate, endDate, licensee } = params
+
+    const billingQuery = new IntegrationlogsQuery()
+    billingQuery.filterByCreatedAt(new Date(startDate), new Date(endDate))
+    billingQuery.sortBy('createdAt', 'asc')
+
+    if (licensee) billingQuery.filterByLicensee(licensee)
+
+    const records = await billingQuery.all()
+
+    io.emit('send_integrationlog', { data: records })
   })
 })
