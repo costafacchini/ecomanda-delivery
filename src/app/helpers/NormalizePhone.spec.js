77c075c050f8f5b643a61ee7abe2a641ca13af9e
Alan Costa Facchini <costafacchini@gmail.com>
2025-09-15 08:46:16 -0300

    Change phone normalizer to understand phone number not mobile (#2254)

--- src/app/helpers/NormalizePhone.spec.js
+++ src/app/helpers/NormalizePhone.spec.js
@@ -26,30 +26,6 @@ describe('NormalizePhone', () => {
       expect(phone.number).toEqual('5512345678901')
     })
 
-    it('normalizes phone adding number 9 if length is 12 numbers', () => {
-      const phone = new NormalizePhone('551234567890@c.us')
-
-      expect(phone.number).toEqual('5512934567890')
-    })
-
-    it('normalizes phone adding Brazil code 55 and 9 if length is 10 numbers', () => {
-      const phone = new NormalizePhone('1234567890@c.us')
-
-      expect(phone.number).toEqual('5512934567890')
-    })
-
-    it('normalizes phone adding Brazil code 55 if length is 11 numbers', () => {
-      const phone = new NormalizePhone('1234567890@c.us')
-
-      expect(phone.number).toEqual('5512934567890')
-    })
-
-    it('normalizes phone if it has invalid chars', () => {
-      const phone = new NormalizePhone('+551A383B7265C34@c.us')
-
-      expect(phone.number).toEqual('5513983726534')
-    })
-
     it('does not normalize phone if length less than 9 numbers', () => {
       const shortPhone = new NormalizePhone('123456789@c.us')
 
@@ -70,6 +46,54 @@ describe('NormalizePhone', () => {
       const phone = new NormalizePhone('+551383726534.@c.us')
 
       expect(phone.number).toEqual('551383726534.')
+    })
+
+    it('normalizes phone if it has invalid chars', () => {
+      const phone = new NormalizePhone('+551A383B7265C34@c.us')
+
+      expect(phone.number).toEqual('5513983726534')
+    })
+
+    describe('about phone types', () => {
+      describe('when the first number after ddd is greather than 5', () => {
+        it('normalizes phone adding number 9 if length is 12 numbers', () => {
+          const phone = new NormalizePhone('551264567890@c.us')
+
+          expect(phone.number).toEqual('5512964567890')
+        })
+
+        it('normalizes phone adding Brazil code 55 and 9 if length is 10 numbers', () => {
+          const phone = new NormalizePhone('1264567890@c.us')
+
+          expect(phone.number).toEqual('5512964567890')
+        })
+
+        it('normalizes phone adding Brazil code 55 if length is 11 numbers', () => {
+          const phone = new NormalizePhone('1264567890@c.us')
+
+          expect(phone.number).toEqual('5512964567890')
+        })
+      })
+
+      describe('when the first number after ddd is less than 6', () => {
+        it('normalizes phone without adding number if length is 12 numbers', () => {
+          const phone = new NormalizePhone('551234567890@c.us')
+
+          expect(phone.number).toEqual('551234567890')
+        })
+
+        it('normalizes phone adding Brazil code 55 if length is 10 numbers', () => {
+          const phone = new NormalizePhone('1234567890@c.us')
+
+          expect(phone.number).toEqual('551234567890')
+        })
+
+        it('normalizes phone adding Brazil code 55 if length is 11 numbers', () => {
+          const phone = new NormalizePhone('1234567890@c.us')
+
+          expect(phone.number).toEqual('551234567890')
+        })
+      })
     })
   })
 
