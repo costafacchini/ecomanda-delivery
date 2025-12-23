4b510d06327f1aa95f6e20cfc8eb78cf67085657
Alan Costa Facchini <costafacchini@gmail.com>
2025-12-23 13:51:32 -0300

    remove disconnect

--- .jest/mongo.js
+++ .jest/mongo.js
@@ -14,20 +14,6 @@ class MongoServerTest {
   }
 
   async disconnect() {
-    if (mongoose.connection.readyState !== 0) {
-      try {
-        await mongoose.disconnect()
-        // eslint-disable-next-line no-empty
-      } catch (error) {
-        const ignorableErrors = ['MongoClientClosedError', 'Connection was force closed', 'client was closed']
-        const isIgnorable =
-          ignorableErrors.includes(error?.name) ||
-          ignorableErrors.includes(error?.message) ||
-          ignorableErrors.some((text) => error?.message?.includes(text))
-
-        if (!isIgnorable) throw error
-      }
-    }
     try {
       const instance = global.__MONGOINSTANCE
       if (instance) {
