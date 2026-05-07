6b68761e8a29c9d448a56f8186c7d94d7a6f1d60
Alan Costa Facchini <costafacchini@gmail.com>
2026-05-07 11:27:21 -0300

    feat(licensees): extract WhatsAppPanel, CartPanel, PagarMePanel, Pedidos10Panel [task-03]

--- client/src/pages/Licensees/scenes/Form/index.js
+++ client/src/pages/Licensees/scenes/Form/index.js
@@ -1,18 +1,14 @@
 import { useState } from 'react'
-import { QRCodeSVG } from 'qrcode.react'
-import { FieldWithError, Form } from '../../../../components/form'
+import { Form } from '../../../../components/form'
 import * as Yup from 'yup'
 import { useNavigate } from 'react-router'
-import {
-  setLicenseeWebhook,
-  getBaileysQr,
-  importLicenseeTemplate,
-  sendLicenseePagarMe,
-  signOrderWebhook,
-} from '../../../../services/licensee'
 import MainPanel from './panels/MainPanel'
 import ChatPanel from './panels/ChatPanel'
 import ChatbotPanel from './panels/ChatbotPanel'
+import WhatsAppPanel from './panels/WhatsAppPanel'
+import CartPanel from './panels/CartPanel'
+import PagarMePanel from './panels/PagarMePanel'
+import Pedidos10Panel from './panels/Pedidos10Panel'
 
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
@@ -71,8 +67,6 @@ const licenseeInitialValues = {
 
 function LicenseeForm({ onSubmit, errors, initialValues, currentUser }) {
   let navigate = useNavigate()
-  const [baileysQr, setBaileysQr] = useState(null)
-  const [baileysStatus, setBaileysStatus] = useState(null)
   const [useChat, setUseChat] = useState(false)
   const [useWhatsapp, setUseWhatsapp] = useState(false)
   const [useCart, setUseCart] = useState(false)
@@ -123,425 +117,38 @@ function LicenseeForm({ onSubmit, errors, initialValues, currentUser }) {
               handleBlur={props.handleBlur}
             />
 
-            <div className='row'>
-              <div className='form-group col-5'>
-                <label htmlFor='whatsappDefault'>Whatsapp padrão</label>
-                <select
-                  value={props.values.whatsappDefault}
-                  className='form-select'
-                  id='whatsappDefault'
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                >
-                  <option value=''></option>
-                  <option value='utalk'>Utalk</option>
-                  <option value='dialog'>Dialog360</option>
-                  <option value='ycloud'>YCloud</option>
-                  <option value='pabbly'>Pabbly</option>
-                  <option value='baileys'>Baileys</option>
-                </select>
-              </div>
-            </div>
+            <WhatsAppPanel
+              values={props.values}
+              errors={props.errors}
+              touched={props.touched}
+              handleChange={props.handleChange}
+              handleBlur={props.handleBlur}
+            />
 
-            <fieldset className='pb-4' disabled={props.values.whatsappDefault === ''}>
-              {props.values.whatsappDefault !== 'baileys' && (
-                <>
-                  <div className='row'>
-                    <div className='form-group col-5'>
-                      <label htmlFor='whatsappToken'>Token do whatsapp</label>
-                      <FieldWithError
-                        id='whatsappToken'
-                        name='whatsappToken'
-                        type='text'
-                        onChange={props.handleChange}
-                        onBlur={props.handleBlur}
-                        value={props.values.whatsappToken}
-                      />
-                    </div>
-                  </div>
+            <CartPanel
+              values={props.values}
+              errors={props.errors}
+              touched={props.touched}
+              handleChange={props.handleChange}
+              handleBlur={props.handleBlur}
+            />
 
-                  <div className='row pb-4'>
-                    <div className='form-group col-5'>
-                      <label htmlFor='whatsappUrl'>Url do whatsapp</label>
-                      <FieldWithError
-                        id='whatsappUrl'
-                        name='whatsappUrl'
-                        type='text'
-                        onChange={props.handleChange}
-                        onBlur={props.handleBlur}
-                        value={props.values.whatsappUrl}
-                      />
-                    </div>
-                  </div>
-                </>
-              )}
-
-              {props.values.whatsappDefault === 'ycloud' && (
-                <div className='row pb-2'>
-                  <div className='col-3'>
-                    <div className='form-check'>
-                      <input
-                        type='checkbox'
-                        className='form-check-input'
-                        id='useFileIDYcloud'
-                        onChange={props.handleChange}
-                        onBlur={props.handleBlur}
-                        checked={props.values.useFileIDYcloud}
-                      />
-                      <label className='form-check-label' htmlFor='useFileIDYcloud'>
-                        Usar ID no YCloud ao invés de URL?
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              )}
-
-              {(props.values.whatsappDefault === 'dialog' || props.values.whatsappDefault === 'ycloud') &&
-                props.values.apiToken && (
-                  <div className='row pb-4'>
-                    <div className='form-group col-3'>
-                      <button
-                        onClick={async (event) => {
-                          event.preventDefault()
-                          await setLicenseeWebhook(props.values)
-                        }}
-                        className='btn btn-info'
-                      >
-                        Configurar Webhook no provedor
-                      </button>
-                    </div>
-
-                    <div className='form-group col-2'>
-                      <button
-                        onClick={async (event) => {
-                          event.preventDefault()
-                          await importLicenseeTemplate(props.values)
-                        }}
-                        className='btn btn-info'
-                      >
-                        Importar templates
-                      </button>
-                    </div>
-                  </div>
-                )}
-
-              {props.values.whatsappDefault === 'baileys' && (
-                <div className='row mt-3 pb-4'>
-                  <div className='form-group col-3'>
-                    <button
-                      onClick={async (event) => {
-                        event.preventDefault()
-                        setBaileysQr(null)
-                        setBaileysStatus(null)
-                        const response = await getBaileysQr(props.values)
-                        if (response.data?.qr) {
-                          setBaileysQr(response.data.qr)
-                        } else {
-                          setBaileysStatus(response.data?.message ?? 'Erro ao gerar QR')
-                        }
-                      }}
-                      className='btn btn-info'
-                    >
-                      Gerar QR Code
-                    </button>
-                  </div>
-                  {baileysQr && (
-                    <div className='form-group col-3'>
-                      <QRCodeSVG value={baileysQr} size={200} />
-                    </div>
-                  )}
-                  {baileysStatus && (
-                    <div className='form-group col-3'>
-                      <p>{baileysStatus}</p>
-                    </div>
-                  )}
-                </div>
-              )}
-            </fieldset>
-
-            <fieldset className='pb-4'>
-              <div className='row'>
-                <div className='form-group col-5'>
-                  <label htmlFor='cartDefault'>Plugin para uso de carrinho de compra</label>
-                  <select
-                    value={props.values.cartDefault}
-                    className='form-select'
-                    id='cartDefault'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                  >
-                    <option value=''></option>
-                    <option value='alloy'>Alloy</option>
-                    <option value='go2go'>Go2Go</option>
-                    <option value='go2go_v2'>Go2Go v2</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className='row pb-2'>
-                <div className='col-3'>
-                  <div className='form-check'>
-                    <input
-                      type='checkbox'
-                      className='form-check-input'
-                      id='useCartGallabox'
-                      onChange={props.handleChange}
-                      onBlur={props.handleBlur}
-                      checked={props.values.useCartGallabox}
-                    />
-                    <label className='form-check-label' htmlFor='useCartGallabox'>
-                      Usa gallabox?
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-5'>
-                  <label htmlFor='unidadeId'>Id da loja</label>
-                  <FieldWithError
-                    id='unidadeId'
-                    name='unidadeId'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.unidadeId}
-                  />
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-5'>
-                  <label htmlFor='statusId'>Id do status do carrinho de compra</label>
-                  <FieldWithError
-                    id='statusId'
-                    name='statusId'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.statusId}
-                  />
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-5'>
-                  <label htmlFor='productFractionals'>Produtos</label>
-                  <div className='pb-2'>
-                    <textarea
-                      id='productFractionals'
-                      name='productFractionals'
-                      className='form-control'
-                      rows={10}
-                      onChange={props.handleChange}
-                      onBlur={props.handleBlur}
-                      value={props.values.productFractionals}
-                    />
-                  </div>
-                </div>
-              </div>
-            </fieldset>
-
-            <fieldset className='pb-4'>
-              <div className='row'>
-                <div className='form-group col-1'>
-                  <label htmlFor='financial_player_fee'>% Taxa</label>
-                  <FieldWithError
-                    id='financial_player_fee'
-                    name='financial_player_fee'
-                    type='number'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.financial_player_fee}
-                  />
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-5'>
-                  <label htmlFor='holder_name'>Nome do titular da conta</label>
-                  <FieldWithError
-                    id='holder_name'
-                    name='holder_name'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.holder_name}
-                  />
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-2'>
-                  <label htmlFor='holder_kind'>Tipo titular da conta</label>
-                  <select
-                    value={props.values.holder_kind}
-                    className='form-select'
-                    id='holder_kind'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                  >
-                    <option value=''></option>
-                    <option value='company'>Jurídica</option>
-                    <option value='individual'>Física</option>
-                  </select>
-                </div>
-
-                <div className='form-group col-3'>
-                  <label htmlFor='holder_document'>Documento titular da conta</label>
-                  <FieldWithError
-                    id='holder_document'
-                    name='holder_document'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.holder_document}
-                  />
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-1'>
-                  <label htmlFor='bank'>Banco</label>
-                  <FieldWithError
-                    id='bank'
-                    name='bank'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.bank}
-                  />
-                </div>
-
-                <div className='form-group col-1'>
-                  <label htmlFor='branch_number'>AG</label>
-                  <FieldWithError
-                    id='branch_number'
-                    name='branch_number'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.branch_number}
-                  />
-                </div>
-
-                <div className='form-group col-1'>
-                  <label htmlFor='branch_check_digit'>DG</label>
-                  <FieldWithError
-                    id='branch_check_digit'
-                    name='branch_check_digit'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.branch_check_digit}
-                  />
-                </div>
-
-                <div className='form-group col-1'>
-                  <label htmlFor='account_number'>Conta</label>
-                  <FieldWithError
-                    id='account_number'
-                    name='account_number'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.account_number}
-                  />
-                </div>
-
-                <div className='form-group col-1'>
-                  <label htmlFor='account_check_digit'>DG</label>
-                  <FieldWithError
-                    id='account_check_digit'
-                    name='account_check_digit'
-                    type='text'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.account_check_digit}
-                  />
-                </div>
-              </div>
-
-              <div className='row pb-4'>
-                <div className='form-group col-2'>
-                  <label htmlFor='account_type'>Tipo da conta</label>
-                  <select
-                    value={props.values.account_type}
-                    className='form-select'
-                    id='account_type'
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                  >
-                    <option value=''></option>
-                    <option value='checking'>Corrente</option>
-                    <option value='savings'>Poupança</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='form-group col-3'>
-                  <button
-                    onClick={async (event) => {
-                      event.preventDefault()
-                      await sendLicenseePagarMe(props.values)
-                    }}
-                    className='btn btn-info'
-                  >
-                    Integrar com a Pagar.Me
-                  </button>
-                </div>
-              </div>
-            </fieldset>
+            <PagarMePanel
+              values={props.values}
+              errors={props.errors}
+              touched={props.touched}
+              handleChange={props.handleChange}
+              handleBlur={props.handleBlur}
+            />
 
             {currentUser && currentUser.isPedidos10 && (
-              <fieldset>
-                <div className='row'>
-                  <div className='form-group col-5'>
-                    <label htmlFor='pedidos10_integrator'>Software Integrador</label>
-                    <select
-                      value={props.values.pedidos10_integrator}
-                      className='form-select'
-                      id='pedidos10_integrator'
-                      onChange={props.handleChange}
-                      onBlur={props.handleBlur}
-                    >
-                      <option value=''></option>
-                    </select>
-                  </div>
-                </div>
-
-                <div className='row'>
-                  <div className='form-group col-5'>
-                    <label htmlFor='pedidos10_integration'>Dados da integração</label>
-                    <div className='pb-2'>
-                      <textarea
-                        id='pedidos10_integration'
-                        name='pedidos10_integration'
-                        className='form-control'
-                        rows={10}
-                        onChange={props.handleChange}
-                        onBlur={props.handleBlur}
-                        value={props.values.pedidos10_integration}
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className='row'>
-                  <div className='form-group col-3'>
-                    <button
-                      onClick={async (event) => {
-                        event.preventDefault()
-                        await signOrderWebhook(props.values)
-                      }}
-                      className='btn btn-info'
-                    >
-                      Assinar Webhook P10
-                    </button>
-                  </div>
-                </div>
-              </fieldset>
+              <Pedidos10Panel
+                values={props.values}
+                errors={props.errors}
+                touched={props.touched}
+                handleChange={props.handleChange}
+                handleBlur={props.handleBlur}
+              />
             )}
 
             {errors && (
