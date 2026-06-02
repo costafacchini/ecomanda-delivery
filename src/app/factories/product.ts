592c463ce541ae3b1762fa9db4f2bc1091c2df58
Alan Costa Facchini <costafacchini@gmail.com>
2026-06-02 11:07:06 -0300

    feat(remove-pdv): delete PDV test factories

--- src/app/factories/product.ts
+++
@@ -1,9 +0,0 @@
-import { Factory } from 'fishery'
-import { licensee } from './licensee'
-
-const product = Factory.define(() => ({
-  name: 'Product 1',
-  licensee: licensee.build(),
-}))
-
-export { product }
