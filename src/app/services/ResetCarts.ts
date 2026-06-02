d42124c4ff9662ffa6a4a8486631160d4ad2327e
Alan Costa Facchini <costafacchini@gmail.com>
2026-06-02 09:40:49 -0300

    feat(remove-pdv): delete ResetCarts service

--- src/app/services/ResetCarts.ts
+++
@@ -1,9 +0,0 @@
-import moment from 'moment-timezone'
-
-async function resetCarts({ cartRepository }: Record<string, any> = {}) {
-  const timeLimit = moment().tz('UTC').subtract(1, 'hour')
-  await cartRepository.updateMany({ concluded: false, createdAt: { $lte: timeLimit } }, { concluded: true })
-  return
-}
-
-export { resetCarts }
